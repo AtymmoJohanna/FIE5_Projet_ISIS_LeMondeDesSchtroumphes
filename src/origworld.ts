5121cec4f045d5c53681852e5a85f3b13bfb3035
AtymmoJohanna <bautrel_johanna.atymmo_kamgaing@etud.univ-jfc.fr>
2026-09-21 11:25:11 +0200

    Creation monde

---
+++ src/origworld.ts
@@ -0,0 +1,14 @@
+import { RatioType } from './graphql';
+export const origworld = {
+name: 'A Nice World 2',
+logo: 'icones/ecolo.jpg',
+money: 0,
+score: 0,
+totalangels: 0,
+activeangels: 0,
+angelbonus: 2,
+lastupdate: 0,
+products: [
+
+],
+};
